d5e65120a96a54adfa99978e976edff7c9311d27
GAEUN YIM <59522019+GAEUNYIM@users.noreply.github.com>
2022-05-17 16:59:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,10 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
 requirements = f.read().splitlines()
 
-with open("README.md", "r") as fh:
-long_description = fh.read()
-
 setuptools.setup(
-name="supergluepretrainednetwork", 
-version="0.0.1",
-
-packages=setuptools.find_packages(),
-
-python_requires='>=3.6',
-install_requires=requirements,
+  name="supergluepretrainednetwork", 
+  version="0.0.1",
+  packages=setuptools.find_packages(),
+  python_requires='>=3.6',
+  install_requires=requirements,
 )
